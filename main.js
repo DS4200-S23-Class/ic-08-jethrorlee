aecbcc0aafb6a5cf91744ca488bde9475f5ce52e
jethro-ronald-lee <99052024+jethro-ronald-lee@users.noreply.github.com>
2023-02-15 23:19:38 -0500

    finished ica

---
+++ main.js
@@ -0,0 +1,52 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom:30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#bar_chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("data/data.csv").then((data) => {
+
+// X axis
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(function(d) { return d.Category; }))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+// Add Y 
+
+// find max value
+const MAX_VAL = d3.max(data, (d) => { return parseInt(d.Value); });
+
+var y = d3.scaleLinear()
+  .domain([0, MAX_VAL])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y));
+
+// Bars
+svg.selectAll("mybar")
+  .data(data)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return x(d.Category); })
+    .attr("y", function(d) { return y(d.Value); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.Value); })
+    .attr("fill", "indigo")
+
+})
